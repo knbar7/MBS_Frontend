0eff0a7c23a54b2347839b93c5a8b5d7204a53db
Kendall Barlow <knbar@MacBook-Pro.local>
2023-10-22 23:15:41 -0700

    complete main functionality

--- src/api/genericFunctions.js
+++ src/api/genericFunctions.js
@@ -45,19 +45,18 @@ export const getData = async (dataType, userId) => {
       // Handle network or other errors
       console.error('An error occurred:', error);
     }
-  };
-
+};
 
 // Generic post function
 export const handleSubmitGeneric = async (endpoint, keys, formData) => {
     const data = {};
 
-    console.log(formData)
-
     for (let key of keys) {
         // Set the value from formData for each key
         data[key] = formData[key];
     }
+
+    console.log(data)
 
     try {
         const response = await axios.post(API_CONFIG.baseUrl + "/" + endpoint, data, {
@@ -75,3 +74,54 @@ export const handleSubmitGeneric = async (endpoint, keys, formData) => {
 };
 
 
+export const handleUserPost = async (newName, newEmail, newPassword) => {
+  let role = 'client';
+  let name = newName;
+  let email = newEmail;
+  let password = newPassword;
+
+  let keys = ['name','email','password','role'];
+  let formData = {
+    name,
+    email,
+    password,
+    role
+  }
+
+  const data = {};
+
+  for (let key of keys) {
+      // Set the value from formData for each key
+      data[key] = formData[key];
+  }
+
+  try {
+      const response = await axios.post(API_CONFIG.baseUrl + "/user", data, {
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      });
+  
+      if (response.status === 201) {
+        const token = response.data.token; // Assuming your backend responds with a 'token' field
+  
+        // Store the token in local storage
+        localStorage.setItem('token', token);
+  
+        // Assuming your user data is available in the response
+        const user = response.data.user; // Replace 'user' with the actual key
+        
+  
+        return user;
+      } else {
+        // Handle login failure (e.g., incorrect credentials)
+        console.error('Login failed');
+      }
+
+      console.log('Form submitted successfully', response.data);
+      // You can add any success handling specific to this form
+  } catch (error) {
+      console.error('Form submission failed', error);
+      // Handle errors specific to this form
+  }
+};
--- src/login.jsx
+++ src/login.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { useAuth } from "./authProvider";
-import { handleLogIn } from "./api/genericFunctions";
+import { handleLogIn, handleSubmitGeneric, handleUserPost } from "./api/genericFunctions";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signInUp, setSignInUp] = useState("log-in");
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const { setUser } = useAuth();
 
   const logInToApp = async (e) => {
@@ -16,28 +21,132 @@ export const Login = () => {
         setUser(user);
       }
     } catch (error) {
-      // Handle any login errors here
       console.error('Login failed:', error);
     }
   };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleInputChange = (e, inputName) => {
+    switch (inputName) {
+      case 'email':
+        setEmail(e.target.value);
+        break;
+      case 'password':
+        setPassword(e.target.value);
+        break;
+      case 'newName':
+        setNewName(e.target.value);
+        break;
+      case 'newEmail':
+        setNewEmail(e.target.value);
+        break;
+      case 'newPassword':
+        setNewPassword(e.target.value);
+        break;
+      case 'confirmPassword':
+        setConfirmPassword(e.target.value);
+        break;
+      default:
+    }
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  const handleCreateAccount = async (e) => {
+    e.preventDefault();
+    let errors = [];
+
+    if(newPassword !== confirmPassword){
+      errors.push(' Passwords do not match');
+    }
+
+    if(!newName){
+      errors.push(' Name is missing')
+    }
+
+    if(!newEmail){
+      errors.push(' Email is missing')
+    }
+
+    if(!newPassword){
+      errors.push(' Password is missing')
+    }
+
+    if(errors.length > 0){
+      window.alert(`${errors}`)
+      throw new Error(`${errors}`)
+    }
+
+    try {
+      const user = await handleUserPost(newName, newEmail, newPassword);
+      if (user) {
+        setUser(user);
+      }
+      if(errors) {
+        window.alert("There is already a user with that email");
+        e.target.reset();
+      }
+    } catch (error) {
+      window.alert('Login failed: ' + error.message);
+    }
+
+  }
+
+  const handleSignInUp = (e) => {
+    setSignInUp(e.target.id);
+  }
 
   return (
     <div className="client-content">
       <div className="daily-items-container">
-        <h1>Welcome! Log-in Below</h1>
-        <form action="submit" className="input-form" onSubmit={logInToApp}>
-          <input type="text" placeholder="email" onChange={handleEmailChange} />
-          <input type="password" placeholder="password" onChange={handlePasswordChange} />
-          <input type="submit" value="Log In" />
-        </form>
+        <div className="sign-in-up-selection">
+          <input 
+            type="radio" 
+            id="log-in" 
+            name="signInUp" 
+            defaultChecked 
+            onChange={handleSignInUp}
+          />
+          <label htmlFor="log-in">Log-in</label>
+          <input 
+            type="radio" 
+            id="create-new" 
+            name="signInUp" 
+            onChange={handleSignInUp}
+          />
+          <label htmlFor="create-new">Create Account</label>
+        </div>
+        {
+          signInUp === 'log-in' ?
+          <>
+            <h1>Welcome! Log-in Below</h1>
+            <form action="submit" className="input-form" onSubmit={logInToApp}>
+              <input type="text" placeholder="email" onChange={(e) => handleInputChange(e, 'email')} />
+              <input type="password" placeholder="password" onChange={(e) => handleInputChange(e, 'password')} />
+              <input type="submit" value="Log In" />
+            </form>
+          </>
+          :
+          <>
+            <h1>Welcome!</h1>
+            <h1>Create Account Below</h1>
+            <form action="submit" className="input-form" onSubmit={(e) => handleCreateAccount(e)}>
+              <div className="form-item">
+                <input type="text" placeholder="Name" onChange={(e) => handleInputChange(e, 'newName')} />
+              </div>
+              <div className="form-item">
+                <input type="email" placeholder="Email" onChange={(e) => handleInputChange(e, 'newEmail')} />
+              </div>
+              <div className="form-item">
+                <input type="password" placeholder="Password" onChange={(e) => handleInputChange(e, 'newPassword')} />
+              </div>
+              <div className="form-item">              
+                <input type="password" placeholder="Confirm Password" onChange={(e) => handleInputChange(e, 'confirmPassword')} />
+                <p className={newPassword === confirmPassword ? "hidden" : "error-message"}>Passwords do not match</p>
+              </div>
+              <div className="form-item">
+                <input type="submit" value="Create Account" />
+              </div>
+            </form>
+          </>
+        }
       </div>
     </div>
   );
